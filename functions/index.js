b082e96aa615b83b63d2115ab0b87aa534b6193d
Jared Reando <jared.reando@streem.pro>
2021-03-03 09:58:28 -0800

    Add continuous integration via GitHub actions.

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,8 @@ const functions = require('firebase-functions')
 
 module.exports = {
   hello: functions.https.onRequest((req, res) => {
-    const subject = req.query.subject || 'World'
-    res.send(`Hello ${subject}!`)
+    const subject = req.query.subject || 'Wurld';
+    const message = req.query.message || '';
+    res.send(`Hello ${subject}! ${message}`);
   }),
 }
